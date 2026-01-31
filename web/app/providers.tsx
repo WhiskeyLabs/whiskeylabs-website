d4c149b1a38529ba5a2e68f95611431cb39dcf1b
harish menon <harish.c@gmail.com>
2026-01-31 18:02:58 -0500

    feat: set dark mode as default theme

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
             {children}
         </NextThemesProvider>
     );
